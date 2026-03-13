22aefcef0a367a11a4e94452afbb71626136851a
danielepucci03 <danielepucci03@gmail.com>
2026-03-13 16:33:34 +0100

    add exercise 3 for bonus

--- arrays_1/02-arrays-bonus/script.js
+++ arrays_1/02-arrays-bonus/script.js
@@ -30,6 +30,12 @@ console.log(longNames);
 
 
 // 3. Rimuovi 'Ed' dall'array teachers
+for (let i = 0; i < teachers.length; i++) {
+  if (teachers[i] === 'Ed') {
+    teachers.splice(i, 1);
+  }
+}
+console.log(teachers);
 
 
 // 4. Verifica se 'Fabio' è presente nell'array teachers
