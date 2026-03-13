9e6c8a5682700d345ed41046d235555c388ff14e
danielepucci03 <danielepucci03@gmail.com>
2026-03-13 16:13:51 +0100

    add exercise 8

--- arrays_1/01-arrays/script.js
+++ arrays_1/01-arrays/script.js
@@ -45,4 +45,6 @@ console.log(lewisIndex);
 
 // 8. Verifica se l'array teachers è vuoto
 // e salva il risultato nella variabile isTeachersEmpty
-const isTeachersEmpty = null;
+const isTeachersEmpty = teachers.length === 0;
+console.log(isTeachersEmpty);
+
